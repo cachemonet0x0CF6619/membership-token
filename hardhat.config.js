5ef66e20ce95bcd045013301b445cb7df46ea940
cachemonet0x0CF6619 <90769841+cachemonet0x0CF6619@users.noreply.github.com>
2021-12-04 13:29:18 -0600

    feat: fat membership token

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,11 +4,7 @@ require('@nomiclabs/hardhat-etherscan');
 require('hardhat-gas-reporter');
 require('solidity-coverage');
 
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
 module.exports = {
-  // defaultNetwork: "hardhat",
   defaultNetwork: 'hardhat',
   networks: {
     hardhat: { chainId: 1337 },
@@ -18,7 +14,7 @@ module.exports = {
     settings: {
       optimizer: {
         enabled: true,
-        runs: 10000,
+        runs: 1,
       },
     },
   },
